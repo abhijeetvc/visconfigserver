87d452ffad116a6dd73d9f61f1558f7b891c9ab9
abhi <abhijeetvc7@gmail.com>
2020-10-05 20:28:25 +0530

    Initial commit

---
+++ src/test/java/com/visconfig/visconfigserver/VisconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visconfig.visconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
